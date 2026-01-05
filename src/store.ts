b540c4d5f0e0bfaf1d515736c43ddcf8dc0d077f
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-05 15:02:08 -0500

    actualizando funciona que obtiene las criptos

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,29 @@
 import { create } from 'zustand'
 import axios from 'axios'
 import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema'
+import type { CryptoCurrency } from './types'
+
+type CryptoStore = {
+  cryptocurrencies: CryptoCurrency[]
+  fetchCryptos: () => Promise<void>
+
+}
 
 async function getCryptos() {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?tsym=USD&limit=20'
   const {data: { Data }} = await axios(url)
   const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-  console.log(result);
-  
+  if(result.success) {
+    return result.data
+  }
 }
 
-export const useCryptoStore = create(() => ({
-  fetchCryptos: () => {
-    getCryptos()
+export const useCryptoStore = create<CryptoStore>((set) => ({
+  cryptocurrencies: [],
+  fetchCryptos: async () => {
+    const cryptocurrencies = await getCryptos()
+    set(() => ({
+      cryptocurrencies
+    }))
   }
 }))
