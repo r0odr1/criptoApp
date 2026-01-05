6fe9ebc5cc4aa1b7cd48c2f3beb1008f038cae3f
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-05 15:23:52 -0500

    creando state para moneda y crypto moneda a cotizar

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,8 +1,21 @@
 import { useCryptoStore } from "../store"
 import { currencies } from "../data"
+import { useState, type ChangeEvent } from "react"
+import type { Pair } from "../types"
 
 export default function CriptoSearchForm() {
   const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+  const [pair, setPair] = useState<Pair>({
+    currency: '',
+    criptocurrency: ''
+  })
+
+  const handleChange = ( e: ChangeEvent<HTMLSelectElement>) => {
+    setPair({
+      ...pair,
+      [e.target.name] : e.target.value
+    })
+  }
 
   return (
     <form className="form">
@@ -11,6 +24,7 @@ export default function CriptoSearchForm() {
         <select
           name="currency"
           id="currency"
+          onChange={handleChange}
         >
           <option value="">-- Seleccione --</option>
           {currencies.map( currency => (
@@ -24,6 +38,7 @@ export default function CriptoSearchForm() {
         <select
           name="ciptocurrency"
           id="ciptocurrency"
+          onChange={handleChange}
         >
           <option value="">-- Seleccione --</option>
           {cryptocurrencies.map( crypto => (
