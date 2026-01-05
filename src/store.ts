31aab1b241ebb40d3133af435586a7b9e1cbe6df
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-05 13:07:45 -0500

    validando la respuesta del API con Zod

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,17 @@
 import { create } from 'zustand'
 import axios from 'axios'
+import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema'
 
 async function getCryptos() {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?tsym=USD&limit=20'
-  const {data: {Data}} = await axios(url)
-  console.log(Data);
+  const {data: { Data }} = await axios(url)
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+  console.log(result);
   
 }
 
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
     getCryptos()
-
   }
 }))
