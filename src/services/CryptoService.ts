c9873c16602a16257970e0f3864d31bf61054bee
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-05 15:53:18 -0500

    Llamando al API para obtener la cotizacion actual

--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -12,5 +12,8 @@ export async function getCryptos() {
 }
 
 export async function fetchCurrencyCryptoPrice(pair: Pair) {
-  console.log(pair);
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.criptocurrency}&tsyms=${pair.currency}`
+  const { data: { DISPLAY } } = await axios(url)
+  console.log(DISPLAY[pair.criptocurrency][pair.currency]);
+  
 }
