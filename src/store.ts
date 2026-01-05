0254385c3e7d8e6373de43c12cbc1c569063ff5c
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-05 13:00:46 -0500

    obteniendo las criptomonedas en zustand

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,16 @@
 import { create } from 'zustand'
+import axios from 'axios'
+
+async function getCryptos() {
+  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?tsym=USD&limit=20'
+  const {data: {Data}} = await axios(url)
+  console.log(Data);
+  
+}
 
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
+    getCryptos()
 
   }
 }))
